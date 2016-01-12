db09ef72d6f5bb07ace817460fdb7b7fa5ef193f
Florence Loi <florence.c.loi@gmail.com>
2016-01-11 17:35:54 -0800

    alphabetized the restuarants

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -15,18 +15,29 @@ def print_restaurant_ratings(filename):
 
     the_file = open(filename)
 
+
     for line in the_file: 
         line = line.rstrip()
         line = line.split(":")
 
-        restaurant_ratings["line[0]"] = line[1]
+        restaurant_ratings[line[0]] = line[1]
 
-        print restaurant_ratings
+    sorted_restaurants = sorted(restaurant_ratings)
+        # print sorted_restaurants
+
+    for i in range(len(sorted_restaurants)):
+        print sorted_restaurants[i] + " is rated at " + restaurant_ratings.get(sorted_restaurants[i])
+        # for restaurant in sorted_restaurants:
+            # print restaurant + " is rated at " + restaurant_ratings.get(restaurant)
+            # for restaurant, ratings in restaurant_ratings.items():
+                # print "%s is rated at %d." % (restaurant, ratings)
 
         # for line[0] not in restaurant_ratings.keys():
             # restaurant_ratings[line[0]] = restaurant_ratings.get("line[0]", 0) + 1
-       
-            
+    
+    the_file.close()
+
+print_restaurant_ratings("scores.txt")            
 
 
 
@@ -36,4 +47,4 @@ def print_restaurant_ratings(filename):
 
 
 
-    filename.close()
+    
